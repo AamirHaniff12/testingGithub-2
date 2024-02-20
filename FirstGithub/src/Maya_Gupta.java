f7619140ca178df4a31682042d07585ce1dec60f
Aamir Haniff <113187563+AamirHaniff12@users.noreply.github.com>
2024-02-20 12:39:20 -0500

    Revert "update"
    This reverts commit 9e5d6a4e2ac6a736f1de520264a5b45ddd173a21.

--- FirstGithub/src/Maya_Gupta.java
+++ FirstGithub/src/Maya_Gupta.java
@@ -1,7 +1,7 @@
 public class Maya_Gupta {
     public static void main(String[] args) {
         System.out.println("hi maya");
-        for(int i= 0; i< 16; i++){
+        for(int i= 0; i<= 16; i++){
             System.out.println("I am "+ i + " years old");
         }
     }
