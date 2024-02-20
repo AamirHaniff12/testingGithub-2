2ddd4470a11678faa20be19c40951d77d159e1cf
Aamir Haniff <113187563+AamirHaniff12@users.noreply.github.com>
2024-02-20 12:21:24 -0500

    i am years old

--- FirstGithub/src/Maya_Gupta.java
+++ FirstGithub/src/Maya_Gupta.java
@@ -1,6 +1,8 @@
 public class Maya_Gupta {
     public static void main(String[] args) {
         System.out.println("hi maya");
-        
+        for(int i= 0; i<= 16; i++){
+            System.out.println("I am "+ i + " years old");
+        }
     }
 }
